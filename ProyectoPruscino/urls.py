0f7a4b35bba97f6faf4553b275adfd9ef87b8196
Julian Sarrelli <julisarrelli@JuliSarrelli.local>
2016-06-22 16:13:27 -0300

    algo1

--- ProyectoPruscino/urls.py
+++ ProyectoPruscino/urls.py
@@ -15,8 +15,10 @@ Including another URLconf
 """
 from django.conf.urls import include,url
 from django.contrib import admin
-from django.conf import settings
-from django.conf.urls.static import static
+
+from django.conf import settings 
+from django.contrib.staticfiles.views import serve 
+
 
 
 urlpatterns = [
@@ -25,8 +27,7 @@ urlpatterns = [
   
 ]
 
-if settings.DEBUG:
-    urlpatterns += static(settings.STATIC_URL,
-                          document_root=settings.STATIC_ROOT)
-    urlpatterns += static(settings.MEDIA_URL,
-                          document_root=settings.MEDIA_ROOT)
+
+if settings.DEBUG: 
+    urlpatterns += [ url(r'^static/(?P<path>.*)$', serve), ] 
+
