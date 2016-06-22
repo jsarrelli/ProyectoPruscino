d874aeb6a39858698c460e9e25a51430e8a0bbbe
Julian Sarrelli <julisarrelli@JuliSarrelli.local>
2016-06-22 15:56:44 -0300

    algo

--- ProyectoPruscino/urls.py
+++ ProyectoPruscino/urls.py
@@ -15,9 +15,12 @@ Including another URLconf
 """
 from django.conf.urls import include,url
 from django.contrib import admin
+from <app> import settings
 
 urlpatterns = [
     url(r'^', include('app.urls')),
     url(r'^admin/', admin.site.urls),
-    
+    urlpatterns += patterns('',
+        (r'^static/(?P<path>.*)$', 'django.views.static.serve', {'document_root': settings.STATIC_ROOT}),
+    )
 ]
